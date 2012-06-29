4f9a5fff4d663abd49d53f2a99a8da2f388bcc66
sandeepreddy92 <d.sreddy1992@gmail.com>
2012-06-29 17:37:52 +0530

    first commit

---
+++ CDTrayEject/CDTrayEject/TrayEject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using MayhemCore;
+
+namespace CDTrayEject
+{
+    [MayhemModule("Eject CD Tray", "Eject the computer's CD tray")]
+    public class TrayEject : ReactionBase
+    {
+        public override void Perform()
+        {
+            mciSendString("set cdaudio door open", null, 0, IntPtr.Zero);
+        }
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA")]
+        public static extern void mciSendString(string lpstrCommand,
+            string lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,8 @@
+CDTrayEject
+
+This contains just one reaction that pops open the CD Tray.
+It could be used in the phone remote event.
+So, when a button on the phone is pressed, the tray is ejected.
+
+Watch a video demonstration here:
+http://www.youtube.com/watch?v=R9qtyfEhiHU
